1af96ad5ab05b7abe510acb47974a8e3c8ea9310
CHOISEUNGYOUN <alchemist718@gmail.com>
2021-01-17 23:47:57 +0900

    add a restaurant repository

--- nuber-eats-backend/src/restaurants/restaurants.module.ts
+++ nuber-eats-backend/src/restaurants/restaurants.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Restaurant } from './entities/restraurant.entity';
 import { RerstaurantResolver } from './restaurants.resolver';
+import { RestaurantService } from './restaurants.service';
 
 @Module({
-  providers: [RerstaurantResolver],
+  imports: [TypeOrmModule.forFeature([Restaurant])],
+  providers: [RerstaurantResolver, RestaurantService],
 })
 export class RestaurantsModule {}
--- nuber-eats-backend/src/restaurants/restaurants.resolver.ts
+++ nuber-eats-backend/src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,14 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
 import { Restaurant } from './entities/restraurant.entity';
+import { RestaurantService } from './restaurants.service';
 
 @Resolver(() => Restaurant)
 export class RerstaurantResolver {
-  @Query((returns) => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
+  constructor(private readonly restaurantService: RestaurantService) {}
+  @Query(() => [Restaurant])
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
   @Mutation(() => Boolean)
   createRestaurant(@Args() CreateRestaurantDto: CreateRestaurantDto): boolean {
